0b50e05cd5cada4542e07cb65d6e4beeb5f8baee
Gowtham Varma Sagi <106369690+GowthamCyro@users.noreply.github.com>
2025-07-22 14:49:36 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,7 +4,7 @@ echo "Stopping container if running..."
 CONTAINER_ID=$(docker ps -q)
 
 if [ -n "$CONTAINER_ID" ]; then
-    echo "Stopping container $CONTAINER_ID..."
+    echo "Stopping container $CONTAINER_ID.."
     docker stop "$CONTAINER_ID"
     docker rm "$CONTAINER_ID"
 else
